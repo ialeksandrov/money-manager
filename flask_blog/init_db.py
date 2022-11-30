d1bdd20335746d7a4f43efa94282076a69c52732
ialeksandrov <ialeksandrov1990@abv.bg>
2022-11-30 23:43:58 +0200

    Update flask deployment, and create postgres deployment to migrate to in the future.

--- flask_blog/init_db.py
+++ flask_blog/init_db.py
@@ -1,20 +1,30 @@
-import sqlite3
+# import sqlite3
+import os
+import psycopg2
 
-connection = sqlite3.connect('database.db')
-
-
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+connection = psycopg2.connect(
+    host='postgres',
+    database='flaskblog',
+    user=os.environ['DB_USERNAME'],
+    password=os.environ['DB_PASSWORD'])
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+cur.execute('DROP TABLE IF EXISTS posts;')
+cur.execute('CREATE TABLE posts (id SERIAL PRIMARY KEY,'
+            'created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,'
+            'title TEXT NOT NULL,'
+            'content TEXT NOT NULL);'
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (%s, %s)",
             ('First Post', 'Content for the first post')
             )
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+cur.execute("INSERT INTO posts (title, content) VALUES (%s, %s)",
             ('Second Post', 'Content for the second post')
             )
 
 connection.commit()
 connection.close()
+cur.close()
