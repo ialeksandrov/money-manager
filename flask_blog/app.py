86fc13c6f5bbbc41c9d20ee9bed2d4d7ab956d38
ialeksandrov <ialeksandrov1990@abv.bg>
2023-06-13 16:23:52 +0300

    Solving some PEP8 notes

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -2,6 +2,7 @@ import psycopg2
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+
 def get_db_connection():
     conn = psycopg2.connect(
         host='postgres',
@@ -13,6 +14,7 @@ def get_db_connection():
     conn.row_factory = psycopg2.extras.DictCursor
     return conn
 
+
 def get_post(post_id):
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -23,6 +25,7 @@ def get_post(post_id):
     if post is None:
         abort(404)
     return post
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'firestarter23'
@@ -38,10 +41,12 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
@@ -62,6 +67,7 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
@@ -84,6 +90,7 @@ def edit(id):
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
+
 
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
